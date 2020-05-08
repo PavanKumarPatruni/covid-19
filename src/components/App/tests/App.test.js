034ee9b9b9afd02e712b4ff83380330048786f8f
Pavan Kumar Patruni <pavankumar.patruni@publicissapient.com>
2020-05-09 01:45:11 +0530

    precommit hook checking

--- src/components/App/tests/App.test.js
+++ src/components/App/tests/App.test.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable extra-rules/no-commented-out-code */
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
 
 test('renders learn react link', () => {
-  // const wrapper = renderer.create(<App />).toJSON();
-  // expect(wrapper).toMatchSnapshot();
+  const wrapper = renderer.create(<App />).toJSON();
+  expect(wrapper).toMatchSnapshot();
 });
