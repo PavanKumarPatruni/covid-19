6eafacb67560a5f402c421d4f0a119f4aeb27e58
Pavan Kumar Patruni <pavankumar.patruni@publicissapient.com>
2020-05-12 17:16:01 +0530

    test coverage improved

--- config/test/jest.config.js
+++ config/test/jest.config.js
@@ -45,7 +45,7 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 90,
-      branches: 89,
+      branches: 90,
       functions: 90,
       lines: 90,
     },
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,7 +12,6 @@ module.exports = {
   },
   mode: 'production',
   optimization: {
-    minimizer: [new TerserPlugin()],
     splitChunks: {
       chunks: 'all',
       minSize: 500,
